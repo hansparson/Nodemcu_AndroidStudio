5def854199f9e0fd6854864337723d66176af2d5
hansparson <hansparson013@gmail.com>
2022-06-21 17:19:27 +0700

    IoT Control Monitoring

--- app/src/main/java/com/hansparson/blacktediot/MainActivity.java
+++ app/src/main/java/com/hansparson/blacktediot/MainActivity.java
@@ -1,5 +1,7 @@
 package com.hansparson.blacktediot;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,12 +9,27 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference Referensi;
+
+    private TextView temperature;
+    private TextView kelembapan;
+    private TextView intensitas_cahaya;
+    private TextView curah_hujan;
 
 
     boolean doubleBackToExitPressedOnce = false;
@@ -36,9 +53,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        temperature = findViewById(R.id.temperature);
+        kelembapan = findViewById(R.id.kelembapan);
+        curah_hujan = findViewById(R.id.curah_hujan);
+        intensitas_cahaya = findViewById(R.id.intensitas_cahaya);
+
+        getdata();
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.navigation_sensor);
-
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -55,6 +78,30 @@ public class MainActivity extends AppCompatActivity {
                         return false;
                     }
                 });
+
+    }
+
+    private void getdata() {
+        Referensi = FirebaseDatabase.getInstance().getReference().child("Sensors");
+        Referensi.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String Nilai_Temperature = snapshot.child("Temperature").getValue().toString();
+                String Nilai_Kelembapan = snapshot.child("Kelembapan").getValue().toString();
+                String Nilai_Cahaya = snapshot.child("Intensitas_Cahaya").getValue().toString();
+                String Nilai_Hujan = snapshot.child("Curah_Hujan").getValue().toString();
+
+                temperature.setText(Nilai_Temperature);
+                kelembapan.setText(Nilai_Kelembapan);
+                intensitas_cahaya.setText(Nilai_Cahaya);
+                curah_hujan.setText(Nilai_Hujan);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
 }
--- app/src/main/java/com/hansparson/blacktediot/RelayControl.java
+++ app/src/main/java/com/hansparson/blacktediot/RelayControl.java
@@ -7,13 +7,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
 
+import com.github.angads25.toggle.interfaces.OnToggledListener;
+import com.github.angads25.toggle.model.ToggleableView;
+import com.github.angads25.toggle.widget.LabeledSwitch;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class RelayControl extends AppCompatActivity {
 
+    DatabaseReference Referensi;
+    private LabeledSwitch Relay1;
+    private LabeledSwitch Relay2;
 
     boolean doubleBackToExitPressedOnce = false;
     @Override
@@ -36,11 +51,31 @@ public class RelayControl extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relay_control);
 
+        Relay1 = findViewById(R.id.relay_1);
+        Relay2 = findViewById(R.id.relay_2);
+
+        getdata();
+
+        Relay1.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                Toast.makeText(RelayControl.this, "relay 1 " +isOn, Toast.LENGTH_SHORT).show();
+                FirebaseDatabase.getInstance().getReference().child("Switch").child("Relay_1").setValue(isOn);
+            }
+        });
+
+        Relay2.setOnToggledListener(new OnToggledListener() {
+            @Override
+            public void onSwitched(ToggleableView toggleableView, boolean isOn) {
+                Toast.makeText(RelayControl.this, "relay 2 " +isOn, Toast.LENGTH_SHORT).show();
+                FirebaseDatabase.getInstance().getReference().child("Switch").child("Relay_2").setValue(isOn);
+            }
+        });
+
+
         BottomNavigationView bottomNavigationView = (BottomNavigationView)
-                findViewById(R.id.bottomNavigationView);
+                        findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setSelectedItemId(R.id.navigation_relay);
-
-
         bottomNavigationView.setOnNavigationItemSelectedListener(
                 new BottomNavigationView.OnNavigationItemSelectedListener() {
                     @Override
@@ -48,7 +83,6 @@ public class RelayControl extends AppCompatActivity {
                         switch (item.getItemId()) {
                             case R.id.navigation_sensor:
                                 startActivity(new Intent(RelayControl.this, MainActivity.class));
-
                                 break;
                             case R.id.navigation_relay:
 
@@ -61,5 +95,24 @@ public class RelayControl extends AppCompatActivity {
                     }
                 });
 
+    }
+
+    private void getdata() {
+        Referensi = FirebaseDatabase.getInstance().getReference().child("Switch");
+        Referensi.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                boolean relay1 = (Boolean) snapshot.child("Relay_1").getValue();
+                boolean relay2 = (Boolean) snapshot.child("Relay_2").getValue();
+
+                Relay1.setOn(relay1);
+                Relay2.setOn(relay2);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
